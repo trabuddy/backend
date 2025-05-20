1228a5d1543eb33dc80ae6e77c8dc44d063e7e1b
jgm0327 <gmjang6349@gmail.com>
2025-05-20 17:44:34 +0900

    feature: 기본 클래스 (#1)

---
+++ src/main/java/com/ssafy/trabuddy/TrabuddyApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.trabuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabuddyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrabuddyApplication.class, args);
+    }
+
+}
