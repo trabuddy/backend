c0c035acc8d0ebf5400a84faf78b02ad9ae9f28a
jgm0327 <gmjang6349@gmail.com>
2025-05-22 16:12:36 +0900

    feature: 프론트 엔드의 헤더에 보여줄 닉네임 보내기  (#10)

--- src/main/java/com/ssafy/trabuddy/domain/member/mapper/MemberMapper.java
+++ src/main/java/com/ssafy/trabuddy/domain/member/mapper/MemberMapper.java
@@ -1,5 +1,7 @@
 package com.ssafy.trabuddy.domain.member.mapper;
 
+import com.ssafy.trabuddy.domain.member.model.dto.GetNicknameResponse;
+import com.ssafy.trabuddy.domain.member.model.dto.GetProfileResponse;
 import com.ssafy.trabuddy.domain.member.model.dto.LoggedInMember;
 import com.ssafy.trabuddy.domain.member.model.entity.MemberEntity;
 import org.mapstruct.Mapper;
@@ -10,4 +12,6 @@ public interface MemberMapper {
     MemberMapper INSTANCE = Mappers.getMapper(MemberMapper.class);
 
     LoggedInMember entityToDto(MemberEntity entity);
+    GetNicknameResponse nicknameToDto(LoggedInMember loggedInMember);
+    GetProfileResponse profileToDto(LoggedInMember loggedInMember);
 }
