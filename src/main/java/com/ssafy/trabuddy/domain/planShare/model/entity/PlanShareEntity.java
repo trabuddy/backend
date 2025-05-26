bb47352e8420314ab737a56871f9eb36639cd644
jgm0327 <gmjang6349@gmail.com>
2025-05-26 10:15:33 +0900

    featrue: 계획 추가 API
    - PlanShare 관련 클래스들: 참여자들 추가하기 위해 생성

--- src/main/java/com/ssafy/trabuddy/domain/planShare/model/entity/PlanShareEntity.java
+++ src/main/java/com/ssafy/trabuddy/domain/planShare/model/entity/PlanShareEntity.java
@@ -1,19 +1,21 @@
 package com.ssafy.trabuddy.domain.planShare.model.entity;
 
+import com.ssafy.trabuddy.domain.member.model.entity.MemberEntity;
+import com.ssafy.trabuddy.domain.member.model.enums.MemberRole;
 import com.ssafy.trabuddy.domain.plan.model.entity.PlanEntity;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Entity
 @EntityListeners(AuditingEntityListener.class)
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "planShare")
+@Table(name = "plan_share")
 public class PlanShareEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,5 +26,24 @@ public class PlanShareEntity {
     @Setter
     private PlanEntity plan;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    @Setter
+    private MemberEntity member;
 
+    @CreatedDate
+    private LocalDateTime joinedAt;
+    private LocalDateTime leftAt;
+
+    private String nickname;
+    @Enumerated(EnumType.STRING)
+    private MemberRole role;
+
+    @Builder
+    public PlanShareEntity(PlanEntity plan, MemberEntity member, String nickname, MemberRole role) {
+        this.plan = plan;
+        this.member = member;
+        this.nickname = nickname;
+        this.role = role;
+    }
 }
